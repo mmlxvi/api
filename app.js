ab3f8397144cbfe22af66860261742c8f8002a3e
mmlxvi <quarterly24@gmail.com>
2014-10-23 18:59:12 -0500

    add

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-//$(document).ready(function() {
+$(document).ready(function() {
+
+var player;
+
+//the question is: what do i put in this function? everything i need to load a
+//random video is set out below the document.ready function. how can i plug in
+//the "onPlayerReady()" function when the #channelChanger button is clicked?
+
+$('#channelChanger').click(function() {
 
 
+})
 
+});
 
 // This code loads the IFrame Player API code asynchronously.
   var tag = document.createElement('script');
@@ -13,14 +23,16 @@
 
 // array of videos for playback:
 
-  var videoLibrary = ['Nw1nRv0jSkY','p24QsMMS0xs', 'xxzPk_qSjrE','o-Uv5ZNk0RE', '4-Kqt5YIXTs', 'a7usMy6EYZI','WTR_rEnW9yA', '5eGqFd7L5F0','WIKqgE4BwAY', 'XyTmXLWCazw'];
+  var videoLibrary = ['Nw1nRv0jSkY','p24QsMMS0xs', 'xxzPk_qSjrE','o-Uv5ZNk0RE',
+  '4-Kqt5YIXTs', 'a7usMy6EYZI','WTR_rEnW9yA', '5eGqFd7L5F0','WIKqgE4BwAY',
+  'XyTmXLWCazw', 'cMqLPDDHtkc'];
 
-  var randomVid = videoLibrary[Math.floor((Math.random() * 10))];
+  var randomVid = videoLibrary[Math.floor((Math.random() * 11))];
 
 
   // 3. This function creates an <iframe> (and YouTube player)
   //    after the API code downloads.
-  var player;
+
   function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
       height: '390',
@@ -39,13 +51,11 @@
   // 4. The API will call this function when the video player is ready.
   function onPlayerReady(event) {
     event.target.playVideo();
+    player.getDuration
   }
 
 
-$('#channelChanger').click(function(){
 
-
-});
 
 //  5. The API calls this function when the player's state changes.
   // The function indicates that when playing a video (state=1),
@@ -53,7 +63,7 @@ $('#channelChanger').click(function(){
   var done = false;
   function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.PLAYING && !done) {
-      setTimeout(stopVideo, 3000);
+      setTimeout(stopVideo, 10000);
       done = true;
     }
   }
